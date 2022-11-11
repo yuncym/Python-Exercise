71d31cc1dae69021b3a6ea267a3988eb5eb8c95c
yuncym <yuncym@gmail.com>
2022-11-12 02:21:09 +0800

    add basic read.py

--- read.py
+++ read.py
@@ -19,6 +19,8 @@ for d in data:
 	#print(sum_len)
 print('每筆留言平均長度為', sum_len/len(data))
 
+
+
 new = []
 #data 清單裡面每一筆每一筆叫出來, 每一個d 都是單獨留言
 #d 是字串, data 是清單
@@ -29,5 +31,30 @@ print('一共有: ', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
+
+# 開始抓有good 的留言
+#good = []
+#for d in data: 
+#	if 'good' in d:
+#		good.append(d) #如果good 有在留言內, 就塞進清單
+
+#'a' in 'abc' -> True
+#'1' in 'abc' -> False
+
+# 下面是快寫法
+# for d in data 
+good = [1 for d in data if 'good' in d]
+print(good)
+
+print('一共有: ', len(good), '筆留言提到good')
+print(good[0])
+
+# 'bad' in d 是個運算式 只有true & false 
+bad = ['bad' in d for d in data]
+print(bad)
+
+#bad = []
+#for d in data:
+#	bad.append('bad' in d)
 
 

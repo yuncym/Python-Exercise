d36a6433fa74fcc14556239acf56284ddfba513b
yuncym <yuncym@gmail.com>
2022-11-16 01:14:55 +0800

    add header

--- products.py
+++ products.py
@@ -1,39 +1,28 @@
 # 讀取現有檔案
 products = []
-
 with open('products.csv', 'r', encoding = 'utf-8') as f:
     for line in f:
         if '商品,價格' in line:
             continue # 繼續
         name, price = line.strip().split(',') #左邊存name
         products.append([name, price])
-
 print(products)
 
-
+# 讓使用者輸入
 while True: 
-	name = input('請輸入商品名稱: ')
-	if name == 'q':  
-		break
-	price = int(input('請輸入商品價格: '))
-	p = []
-	# p.append(name)
-	# p.append(price)
-	#p = [name, price]  # 這可以取代上面兩行
-	#products.append(p)
-	products.append([name, price])
-
+    name = input('請輸入商品名稱: ')
+    if name == 'q':  
+        break
+    price = int(input('請輸入商品價格: '))
+    price = int(price)
+    products.append([name, price])
 print(products)  # 印出有多少商品
 
+# 印出所有購買紀錄
 for p in products:
-	#print(p)
     print(p[0], '的價格是', p[1], '元')
 
-#print(products[0][0])
-#print(products[0][1])
-# 'abc' + '123' = 'abc123'
-# 'abc' * 3 = 'abcabcabc'
-
+# 寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:  # 提示要用共通的utf-8 編碼
 	f.write('商品, 價格\n') # 寫入欄位名稱
 	for p in products:
